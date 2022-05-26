3d0391d46cb4e04b1ae05e3602c3d549b201efa1
Michael Cohen <miccoh1994@gmail.com>
2022-05-26 10:03:46 +0200

    fix typo in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import dts from 'vite-plugin-dts'
-import { EsLinter, linterPlugin } from 'vite-plugin-linter'
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+import { EsLinter, linterPlugin } from 'vite-plugin-linter';
 
 // https://vitejs.dev/config/
-export default defineConfig(configEnv => ({
+export default defineConfig((configEnv) => ({
   plugins: [
     react(),
     tsConfigPaths(),
     linterPlugin({
-      include: ['./example}/**/*.{ts,tsx}'],
+      include: ['./example/**/*.{ts,tsx}'],
       linters: [new EsLinter({ configEnv })],
     }),
     dts({
@@ -32,4 +32,4 @@ export default defineConfig(configEnv => ({
       external: ['react'],
     },
   },
-}))
+}));
